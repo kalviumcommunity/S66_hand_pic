5c581521c56d059b4079e9e55c31168bddfb0171
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-02-13 13:59:01 +0530

    Added React landing page for ASAP

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to HandyPic</h1>
+      <p>This project is all about hands – funny, creative, and unique hand pictures! Users can upload their hand photos,
+        view others submissions, and vote for the best ones.
+        It’s a fun way to celebrate creativity and share interesting hand moments with the community.</p>
+    </div>
+  );
+}
