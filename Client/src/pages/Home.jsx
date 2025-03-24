10b7fc560cc9ca19a4fc5493e12ae3bbf12fb2bd
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-03-24 14:16:58 +0530

    Add auth endpoints with login and logout functionality

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-export default function AsapEntity() {
+import Navbar from "../components/Navbar";
+
+export default function Home() {
     return (
-        <div className="w-3/4 mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
+    <>
+        <Navbar/>
+        <div className="w-3/4 mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">    
         <h2 className="text-2xl font-bold text-gray-800 mb-4">Handscape</h2>
         <p className="text-gray-600">
             This project is all about hands – funny, creative, and unique hand pictures! Users can upload their hand photos,
             view other&apos;s submissions, and vote for the best ones. It&apos;s a fun way to celebrate creativity and share interesting hand moments with the community.
         </p>
         </div>
+        </>
     );
 }
