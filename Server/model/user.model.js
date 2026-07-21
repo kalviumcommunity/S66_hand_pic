f4e435ee2fd3017df1433899d46a2e20451b0d75
Abhinavv15 <abhinavdhanesh15@gmail.com>
2026-07-21 13:44:05 +0530

    Update user.model.js with timestamps, string trimming, and email lowercasing

--- Server/model/user.model.js
+++ Server/model/user.model.js
@@ -2,27 +2,32 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: {
-        type:String,
+        type: String,
         required: true,
         unique: true,
+        trim: true,
     },
     password: {
-        type:String,
+        type: String,
         required: true,
     },
     email: {
-        type:String,
+        type: String,
         required: true,
         unique: true,
+        lowercase: true,
+        trim: true,
     },
     age: {
-        type:Number,
+        type: Number,
         required: true,
     },
     profilePicture: {
         type: String,
         default: ""
     }
+}, {
+    timestamps: true
 });
 
 const UserModel = mongoose.model("Users", userSchema);
